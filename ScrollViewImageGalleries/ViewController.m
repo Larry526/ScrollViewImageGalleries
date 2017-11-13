9bf08285af43e85acc83798e725423c0d2f62e8d
Larry Luk <larryluk526@gmail.com>
2017-11-13 14:02:36 -0500

    part 1 complete

--- ScrollViewImageGalleries/ViewController.m
+++ ScrollViewImageGalleries/ViewController.m
@@ -8,7 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIScrollView *imageScrollView;
 
 @end
 
@@ -16,14 +17,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor blackColor];
+    self.imageScrollView.contentSize = CGSizeMake(self.view.bounds.size.width*3, self.imageScrollView.bounds.size.height);
+    
+    CGRect frame1 = CGRectMake(0,0,self.view.bounds.size.width,self.imageScrollView.bounds.size.height);
+    UIImageView *image1 = [[UIImageView alloc]initWithFrame:frame1];
+    image1.image = [UIImage imageNamed:@"Lighthouse-in-Field"];
+    [self.imageScrollView addSubview:image1];
+    
+    CGRect frame2 = CGRectMake(self.view.bounds.size.width,0,self.view.bounds.size.width,self.imageScrollView.bounds.size.height);
+    UIImageView *image2 = [[UIImageView alloc]initWithFrame:frame2];
+    image2.image = [UIImage imageNamed:@"Lighthouse-night"];
+    [self.imageScrollView addSubview:image2];
+    
+    CGRect frame3 = CGRectMake(self.view.bounds.size.width*2,0,self.view.bounds.size.width,self.imageScrollView.bounds.size.height);
+    UIImageView *image3 = [[UIImageView alloc]initWithFrame:frame3];
+    image3.image = [UIImage imageNamed:@"Lighthouse-zoomed"];
+    [self.imageScrollView addSubview:image3];
+    
+    self.imageScrollView.pagingEnabled = YES;
+    
+
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 
 @end
